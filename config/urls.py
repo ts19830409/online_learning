3d356bb65a5402e3040d1348019d1754facf7a8a
Tsoy Aleksey <i2705@rambler.ru>
2026-05-16 14:50:03 +1000

    Исправлены недочеты по домашней работе 1, сделана домашняя работа 2

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from lms.views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
+from users.views import ProfileView
+from users.views import PaymentListView
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
-    path('api/lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
+	path('admin/', admin.site.urls),
+	path('api/', include(router.urls)),
+	path('api/profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+	path('api/lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
+	path('api/lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
+]
+
+urlpatterns += [
+    path('api/payments/', PaymentListView.as_view(), name='payment-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['course', 'lesson', 'payment_method']
+    ordering_fields = ['payment_date']
