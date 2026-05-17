5e4455629e7f1afed7c9ac8e80bc2312f604eae1
Tsoy Aleksey <i2705@rambler.ru>
2026-05-17 13:54:56 +1000

    Исправлены недочеты по домашней работе 2, сделана домашняя работа 3

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from lms.views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 from users.views import ProfileView
 from users.views import PaymentListView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
@@ -18,4 +19,9 @@ urlpatterns = [
 
 urlpatterns += [
     path('api/payments/', PaymentListView.as_view(), name='payment-list'),
+]
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- lms/views.py
+++ lms/views.py
@@ -1,18 +1,52 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator, IsOwner
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+	queryset = Course.objects.all()
+	serializer_class = CourseSerializer
+	
+	def get_permissions(self):
+		if self.action == 'create':
+			self.permission_classes = [IsAuthenticated, ~IsModerator]
+		elif self.action in ['update', 'partial_update', 'retrieve']:
+			self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+		elif self.action == 'destroy':
+			self.permission_classes = [IsAuthenticated, IsOwner]
+		elif self.action == 'list':
+			self.permission_classes = [IsAuthenticated]
+		return [permission() for permission in self.permission_classes]
+	
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
 
 
 class LessonListCreateView(generics.ListCreateAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
+	queryset = Lesson.objects.all()
+	serializer_class = LessonSerializer
+	permission_classes = [IsAuthenticated]
+	
+	def get_permissions(self):
+		if self.request.method == 'POST':
+			self.permission_classes = [IsAuthenticated, ~IsModerator]
+		return [permission() for permission in self.permission_classes]
+	
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
 
 
 class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
+	queryset = Lesson.objects.all()
+	serializer_class = LessonSerializer
+	
+	def get_permissions(self):
+		if self.request.method in ['PUT', 'PATCH']:
+			self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+		elif self.request.method == 'DELETE':
+			self.permission_classes = [IsAuthenticated, IsOwner]
+		else:
+			self.permission_classes = [IsAuthenticated]
+		return [permission() for permission in self.permission_classes]
